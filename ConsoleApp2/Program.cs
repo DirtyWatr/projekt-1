dcbcacb22f8cf23351e6c551dbbc03ed05f96430
DirtyWatr <zeed.hussen@elev.ga.ntig.se>
2022-09-21 09:43:37 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        }
+    }
+}
